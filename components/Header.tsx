3969472abf55b1a56c8498b0af9cbb43527e7ad1
Francisco González <devfranciscogonzalez@gmail.com>
2024-10-27 02:23:59 -0300

    Refactor CSS variables and import statements

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,14 @@ import { Rocket, Menu, X } from "lucide-react";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  // outline outline-offset-1 outline-1 outline-primary
   return (
-    <header className="px-4 lg:px-6 h-14 border-b border-gray-200 flex">
-      <div className="flex items-center w-full mx-auto max-w-2xl">
+    <header className="flex w-full fixed px-4 lg:px-6 pt-4 h-16 z-50">
+      <div className="absolute top-0 left-0 w-full h-4 backdrop-blur-sm"></div>
+      <section className="flex items-center w-full max-w-5xl mx-auto px-4 bg-primary-foreground/90 backdrop-blur-sm rounded shadow-md shadow-primary/15">
         <Link className="flex items-center justify-center" href="#">
-          <Rocket className="h-6 w-6 mr-2 text-[#1a73e8]" />
-          <span className="font-bold text-[#1a73e8]">AppSheet Dev</span>
+          <Rocket className="h-6 w-6 mr-2 text-primary" />
+          <span className="font-medium bg-gradient-to-r from-primary bg-clip-text text-primary/50">Diego Soluciones Web</span>
         </Link>
         <nav
           className={`ml-auto flex gap-4 sm:gap-6 ${
@@ -21,25 +22,25 @@ export default function Header() {
           } md:flex flex-col md:flex-row absolute md:relative top-14 md:top-0 left-0 right-0 bg-white md:bg-transparent p-4 md:p-0 border-b md:border-b-0 border-gray-200`}
         >
           <Link
-            className="text-sm font-medium hover:text-[#1a73e8]"
+            className="text-sm font-light text-secondary-foreground hover:text-primary"
             href="#services"
           >
             Servicios
           </Link>
           <Link
-            className="text-sm font-medium hover:text-[#1a73e8]"
+            className="text-sm font-light text-secondary-foreground hover:text-primary"
             href="#features"
           >
             Características
           </Link>
           <Link
-            className="text-sm font-medium hover:text-[#1a73e8]"
+            className="text-sm font-light text-secondary-foreground hover:text-primary"
             href="#faq"
           >
             FAQ
           </Link>
           <Link
-            className="text-sm font-medium hover:text-[#1a73e8]"
+            className="text-sm font-light text-secondary-foreground hover:text-primary"
             href="#contact"
           >
             Contacto
@@ -57,7 +58,7 @@ export default function Header() {
             <Menu className="h-6 w-6 text-foreground" />
           )}
         </Button>
-      </div>
+      </section>
     </header>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import ArrowSvg from "./icons/ArrowSvg";
 
 export default function Hero() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 text-foreground  ">
+    <section className="w-full max-w-5xl py-12 md:py-24 lg:py-32 xl:py-48 text-center">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2 animate-fade-up">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              Desarrollo No-Code con Google AppSheet
+            <h1 className="text-3xl font-medium tracking-tight text-secondary-foreground sm:text-4xl md:text-5xl lg:text-7xl">
+              Crece con{" "}
+              <span className="relative whitespace-nowrap">
+                <ArrowSvg className="absolute -bottom-1 left-0 w-full text-primary rotate-180" />
+                <span className="bg-gradient-to-r from-primary bg-clip-text text-primary/50">
+                  soluciones
+                </span>
+              </span>{" "}
+              diseñadas para tu negocio
             </h1>
             <p className="mx-auto max-w-[700px] md:text-xl text-blue-100">
               Transforme sus ideas en aplicaciones funcionales sin escribir
