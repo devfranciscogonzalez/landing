80de71d9c6581db6c4c057727071a3b5cf26a61b
Francisco González <devfranciscogonzalez@gmail.com>
2024-11-03 00:33:39 -0300

    Refactor Contact and FAQ components: enhance layout, add typography elements, and improve accessibility features

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,27 +1,53 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { Send } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Send } from "lucide-react";
+import { TypographyH2, TypographyLead, TypographyP } from "./ui/typography";
 
 export default function Contact() {
   return (
-    <section id="contact" className="w-full py-12 md:py-24 lg:py-32">
-      <div className="container px-4 md:px-6">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8 text-[#1a73e8]">
-          Contáctame
-        </h2>
+    <section
+      id="contact"
+      className="flex w-full max-w-5xl py-12 md:py-24 lg:py-32"
+    >
+      <div className="w-5/12 px-4 md:px-6">
         <div className="mx-auto max-w-lg">
           <form className="space-y-4">
             <Input placeholder="Nombre" />
             <Input type="email" placeholder="Email" />
             <Textarea placeholder="Mensaje" />
-            <Button className="w-full bg-[#1a73e8] hover:bg-blue-700">
+            <Button className="w-full">
               Enviar mensaje
               <Send className="ml-2 h-4 w-4" />
             </Button>
           </form>
         </div>
+        <TypographyP>
+          Si prefieres, puedes escribirme directamente al siguiente correo
+          electrónico:
+        </TypographyP>
+        <div>
+          <Button variant="secondary" className="mt-4">
+            franciscogonzalez
+          </Button>
+          <div>
+            <Button variant="" className="mt-4">
+            </Button>
+            <Button variant="secondary" className="mt-4">
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="w-7/12 px-4 md:px-6">
+        <TypographyP>Contáctanos</TypographyP>
+        <TypographyH2 className="bg-gradient-to-l from-blue-800 to-primary bg-clip-text text-transparent mt-0">
+          Estaremos encantados de ayudarte.
+        </TypographyH2>
+        <TypographyLead className="mt-4 text-secondary-foreground">
+          ¿Tienes alguna pregunta o necesitas más información? Escríbenos y
+          responderemos lo antes posible.
+        </TypographyLead>
       </div>
     </section>
-  )
+  );
 }
--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -4,21 +4,35 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { TypographyH2, TypographyLead, TypographyP } from "./ui/typography";
 
 export default function FAQ() {
   return (
-    <section id="faq" className="w-full py-12 md:py-24 lg:py-32 bg-primary">
-      <div className="container px-4 md:px-6">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
-          Preguntas Frecuentes
-        </h2>
+    <section
+      id="faq"
+      className="flex justify-center w-full px-6 pb-32 text-center bg-primary"
+    >
+      <div className="flex gap-6 w-full max-w-5xl px-4 md:px-6">
+        <div className="flex flex-col text-left ">
+          <TypographyLead className="text-secondary/30">
+            Preguntas Frecuentes
+          </TypographyLead>
+          <TypographyH2 className="bg-gradient-to-l from-slate-300 to-white bg-clip-text text-transparent mt-0">
+            Todo lo que necesitas saber
+          </TypographyH2>
+        </div>
         <Accordion
-          type="single"
-          collapsible
-          className="w-full max-w-3xl mx-auto"
+          type="multiple"
+          className="w-full max-w-3xl mx-auto text-secondary text-left"
         >
-          <AccordionItem value="item-1">
-            <AccordionTrigger>¿Qué es Google AppSheet?</AccordionTrigger>
+          <TypographyP className="text-destructive/80">General</TypographyP>
+          <AccordionItem
+            value="item-1"
+            className="border-b border-secondary/30"
+          >
+            <AccordionTrigger className="data-[state=open]:font-extrabold">
+              ¿Qué es Google AppSheet?
+            </AccordionTrigger>
             <AccordionContent>
               Google AppSheet es una plataforma de desarrollo no-code que
               permite crear aplicaciones móviles y web sin necesidad de escribir
@@ -26,7 +40,10 @@ export default function FAQ() {
               lógica y la estructura de la aplicación.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-2">
+          <AccordionItem
+            value="item-2"
+            className="border-b border-secondary/30 mt-4"
+          >
             <AccordionTrigger>
               ¿Cuánto tiempo lleva desarrollar una aplicación con AppSheet?
             </AccordionTrigger>
@@ -37,7 +54,7 @@ export default function FAQ() {
               desarrollo tradicional.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-3">
+          <AccordionItem value="item-3" className="border-none">
             <AccordionTrigger>
               ¿Puedo integrar AppSheet con otros servicios?
             </AccordionTrigger>
@@ -48,15 +65,56 @@ export default function FAQ() {
               utilizando APIs.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-4">
+          <TypographyP className="text-destructive/80">Soporte</TypographyP>
+          <AccordionItem
+            value="item-4"
+            className="border-b border-secondary/30 mt-4"
+          >
             <AccordionTrigger>
-              ¿Necesito conocimientos de programación para usar AppSheet?
+              ¿Realizas reuniones de soporte a distancia?
             </AccordionTrigger>
             <AccordionContent>
-              No se requieren conocimientos de programación para usar AppSheet.
-              Sin embargo, entender conceptos básicos de bases de datos y lógica
-              de negocio puede ser beneficioso para crear aplicaciones más
-              complejas.
+              Sí, realizo reuniones de soporte a distancia utilizando
+              plataformas como Zoom, Google Meet, y otras opciones de
+              telecomunicación según la preferencia del cliente. Esto permite un
+              soporte flexible y adaptado a tus necesidades.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-6" className="border-none">
+            <AccordionTrigger>
+              ¿Ofreces soporte después de la entrega del proyecto?
+            </AccordionTrigger>
+            <AccordionContent>
+              Sí, ofrezco soporte posterior a la entrega del proyecto para
+              asegurar que la implementación sea exitosa y cualquier inquietud
+              sea resuelta. Los detalles del soporte pueden adaptarse según las
+              necesidades y los acuerdos establecidos.
+            </AccordionContent>
+          </AccordionItem>
+          <TypographyP className="text-destructive/80">
+            Metodo de Pago
+          </TypographyP>
+          <AccordionItem
+            value="item-6"
+            className="border-b border-secondary/30 mt-4"
+          >
+            <AccordionTrigger>¿Qué medios de pago aceptas?</AccordionTrigger>
+            <AccordionContent>
+              Acepto una  variedad de medios de pago, incluyendo transferencias
+              bancarias, tarjetas de crédito y débito, y otras opciones
+              populares. Mi objetivo es facilitar el proceso de pago para que
+              sea conveniente para ti.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-7" className="border-none">
+            <AccordionTrigger>
+              ¿Es posible realizar pagos en cuotas?
+            </AccordionTrigger>
+            <AccordionContent>
+              Sí, ofrezco la opción de pagos en cuotas en ciertos casos, para
+              facilitar el proceso de pago según la naturaleza del proyecto y
+              las necesidades del cliente. Los términos pueden ajustarse en base
+              a un acuerdo previo.
             </AccordionContent>
           </AccordionItem>
         </Accordion>
